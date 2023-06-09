4d7861eabe04c60982e04c994db866c042cae0f7
Avantikaraj6 <126805949+Avantikaraj6@users.noreply.github.com>
2023-06-09 08:22:56 +0530

    Pizza- Initial Commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import TechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 150.99,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 110.99,
+  },
+  {
+    name: "Tech Special Pizza",
+    image: TechSpecial,
+    price: 256.53,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 170.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 149.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 1997.99,
+  },
+];
